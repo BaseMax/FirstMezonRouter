8e6391e64060a7f79baa4d0b7f09a5250fd155d1
Max Base <MaxBaseCode@gmail.com>
2021-12-15 18:50:56 +0330

    run routes

--- index.php
+++ index.php
@@ -16,17 +16,21 @@ $router->addRoute('/[i:digit]-[s:name].html', function(string $route, array $par
 }, 'GET');
 
 // $router->addRoute('/static-route/', 'MySite::someStaticMethod');
-$router->addRoute('/contacts/', function(){}, 'POST'); // this handler will be called for POST requests
-$router->addRoute('/contacts/', function(){}, 'GET');  // this handler will be called for GET requests
-$router->addRoute('/contacts/', function(){}, 'PUT');  // this handler will be called for PUT requests
-$router->addRoute('/contacts/', function(){}, 'DELETE');  // this handler will be called for DELETE requests
-$router->addRoute('/contacts/', function(){}, 'OPTION');  // this handler will be called for OPTION requests
-$router->addRoute('/contacts/', function(){}, 'PATCH');  // this handler will be called for PATCH requests
+// $router->addRoute('/contacts/', function(){}, 'POST'); // this handler will be called for POST requests
+// $router->addRoute('/contacts/', function(){}, 'GET');  // this handler will be called for GET requests
+// $router->addRoute('/contacts/', function(){}, 'PUT');  // this handler will be called for PUT requests
+// $router->addRoute('/contacts/', function(){}, 'DELETE');  // this handler will be called for DELETE requests
+// $router->addRoute('/contacts/', function(){}, 'OPTION');  // this handler will be called for OPTION requests
+// $router->addRoute('/contacts/', function(){}, 'PATCH');  // this handler will be called for PATCH requests
 // $router->addRoute('/contacts/', function(){});
-$router->addRoute('/*/', function(){});
-$router->addRoute('/index/', function(){});
+// $router->addRoute('/*/', function(){});
+// $router->addRoute('/index/', function(){});
+
+// Test it!
+// $callback = $router->getCallback('/');
+// $callback = $router->getCallback('/100-test.html');
+// var_dump($callback);
 
 // Run it!
-// $callback = $router->getCallback('/');
-$callback = $router->getCallback('/100-test.html');
-var_dump($callback);
+$router->callRoute('/');
+$router->callRoute('/100-test.html');
